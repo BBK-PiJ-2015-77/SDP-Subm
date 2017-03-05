40e325e463fbe32adc13db4ba3f3e016b183015e
Thomas McGarry <mcgarry_tom@hotmail.com>
2017-03-05 18:05:06 +0000

    ConcreteProduct interface done

---
+++ exercises/Week07/src/main/scala/factory/ConcreteProduct.java
@@ -0,0 +1,12 @@
+package factory;
+
+/**
+ * Created by thomasmcgarry on 05/03/2017.
+ */
+public class ConcreteProduct implements Product{
+
+    public String getDescription() { return "Concrete"; }
+
+    public double getCost() { return 50.0; }
+
+}
