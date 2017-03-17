e2d389313d876c46f9fcca08969c817d94f1c765
Thomas McGarry <mcgarry_tom@hotmail.com>
2017-03-17 16:03:57 +0000

    mul done

--- coursework/cw-one/SML/src/sml/Translator.java
+++ coursework/cw-one/SML/src/sml/Translator.java
@@ -100,6 +100,12 @@ public class Translator {
                 s1 = scanInt();
                 s2 = scanInt();
                 return new SubInstruction(label, r, s1, s2);
+            case "mul":
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new MulInstruction(label, r, s1, s2);
+            //Do we need to have a case for each operation?
         }
 
         // You will have to write code here for the other instructions.
--- coursework/cw-one/test1.sml
+++ coursework/cw-one/test1.sml
@@ -2,3 +2,5 @@ L1 lin 10 66
 L2 add 10 10 10
 L3 lin 9 100
 L4 sub 8 10 9
+L5 lin 7 2
+L6 mul 6 7 8
