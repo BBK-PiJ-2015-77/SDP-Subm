6c23c4e1c6d48adbeeec8873ab3fa19031d1cb9b
Thomas McGarry <mcgarry_tom@hotmail.com>
2017-04-30 16:43:36 +0100

    latest

---
+++ coursework/cw-two/src/main/scala/bc/Iadd.scala
@@ -0,0 +1,27 @@
+package bc
+
+import vm.VirtualMachine
+
+/**
+  * Created by thomasmcgarry on 30/04/2017.
+  */
+class Iadd extends ByteCode{
+  /**
+    * A unique byte value representing the bytecode. An implementation
+    * will set this to the bytecode corresponding to the name of the
+    * bytecode in [[ByteCodeValues]]
+    */
+  override val code: Byte = bytecode("iadd")
+
+  /**
+    * Returns a new [[VirtualMachine]] after executing this bytecode operation.
+    *
+    * @param vm the initial virtual machine
+    * @return a new virtual machine
+    */
+  override def execute(vm: VirtualMachine): VirtualMachine = {
+    val x = vm.pop()
+    val y = vm.pop()
+    vm.push(x._1 + y._1)
+  }
+}
--- coursework/cw-two/src/main/scala/bc/Isub.scala
+++ coursework/cw-two/src/main/scala/bc/Isub.scala
@@ -5,13 +5,13 @@ import vm.VirtualMachine
 /**
   * Created by thomasmcgarry on 30/04/2017.
   */
-class Iadd extends ByteCode{
+class Isub extends ByteCode{
   /**
     * A unique byte value representing the bytecode. An implementation
     * will set this to the bytecode corresponding to the name of the
     * bytecode in [[ByteCodeValues]]
     */
-  override val code: Byte = bytecode("iconst")
+  override val code: Byte = bytecode("isub")
 
   /**
     * Returns a new [[VirtualMachine]] after executing this bytecode operation.
@@ -20,8 +20,8 @@ class Iadd extends ByteCode{
     * @return a new virtual machine
     */
   override def execute(vm: VirtualMachine): VirtualMachine = {
-    val x: Int = vm.pop()
-    val y: Int = vm.pop()
-    vm.push(x + y)
+    val x = vm.pop()
+    val y = vm.pop()
+    vm.push(x._1 - y._1)
   }
 }
