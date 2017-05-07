917912dc11c3cb6ae8fb01a7a3ef514ece2b2314
Thomas McGarry <mcgarry_tom@hotmail.com>
2017-05-07 11:53:55 +0100

    latest

--- exercises/week08/src/main/scala/composite/HtmlElement.scala
+++ exercises/week08/src/main/scala/composite/HtmlElement.scala
@@ -6,10 +6,13 @@ case class HtmlElement(s: String) extends HtmlTag(s) {
 
   var startTag: String
   var endTag: String
+  var bodyTag: String
 
   override def setStartTag(tag: String): Unit = {startTag = tag}
 
   override def setEndTag(tag: String): Unit = {endTag = tag}
+
+  override def setTagBody(tagBody: String): Unit = {bodyTag = tagBody}
 
   override def generateHtml: Unit = ???
 
--- exercises/week08/src/main/scala/composite/HtmlParentElement.scala
+++ exercises/week08/src/main/scala/composite/HtmlParentElement.scala
@@ -6,14 +6,18 @@ case class HtmlParentElement(s: String) extends HtmlTag(s) {
   var startTag: String
   var endTag: String
   var childElements: List[HtmlTag] = List.empty
+  var html: String
 
   override def setStartTag(tag: String): Unit = {startTag = tag}
 
   override def setEndTag(tag: String): Unit = {endTag = tag}
 
-  override def generateHtml: Unit = ???
+  override def generateHtml: Unit = {
+    html = s"$startTag" + s"\n$endTag"
+  }
 
-  override def addChildTag(htmlTag: HtmlTag): Unit = {childElements += htmlTag}
-//abc
+  override def addChildTag(htmlTag: HtmlTag): Unit = {childElements :+= htmlTag}
+
+  override def removeChildTag(htmlTag: HtmlTag): Unit = {childElements = childElements.filter(_ != htmlTag)}
 
 }
