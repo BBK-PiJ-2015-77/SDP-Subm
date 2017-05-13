e9a286a22b9e7fbf56f1543cbc524aba7a75181d
Thomas McGarry <mcgarry_tom@hotmail.com>
2017-05-13 12:24:59 +0100

    latest

--- exercises/week08/src/main/scala/builder/CarDirector.scala
+++ exercises/week08/src/main/scala/builder/CarDirector.scala
@@ -3,6 +3,17 @@ package builder
 // TODO
 
 case class CarDirector(carBuilder: CarBuilder) {
-  def build = ???
+
+  private val cb: CarBuilder = carBuilder
+
+  def build: Unit = {
+    cb.buildBodyStyle
+    cb.buildPower
+    cb.buildEngine
+    cb.buildBreaks
+    cb.buildSeats
+    cb.buildWindows
+    cb.buildFuelType
+  }
 
 }
